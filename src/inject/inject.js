4c32d384876ee4e6e8ce17d969cf82201df09705
Alexey Belozerov <asbelozerov@gmail.com>
2013-07-18 16:22:45 +0700

    Version 1.0.5

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,9 +3,18 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+        // Because of bug in Chrome match_excludes with CSS files making CSS inline
+        $(document.body).append("<style type='text/css'>"
+            + "a#gbg1,"
+            + "#sb-button-notify.yt-uix-button"
+            + "{"
+            + "    display: none;"
+            + "}"
+            + "</style>")
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		$("a#gbg1").css("display", "none");
+        $("#sb-button-notify.yt-uix-button").css("display", "none");
 		// ----------------------------------------------------------
 
 	}
