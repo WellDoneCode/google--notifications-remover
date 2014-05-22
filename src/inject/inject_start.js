fddfd82b23e26274c0a7962570b63631fddc7d81
Alexey Belozerov <asbelozerov@gmail.com>
2014-05-22 17:08:12 +0700

    Youtube fixed. Unnecessary scripts are deleted. Version 1.0.16

--- src/inject/inject_start.js
+++ src/inject/inject_start.js
@@ -1,82 +1,43 @@
-// Observer which waits when <body> added to document
-var observerForBody = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        for (var i = 0; i < mutation.addedNodes.length; i++) {
-            var addedNode = mutation.addedNodes[i];
+var css =
+    ".yt-uix-clickcard #sb-button-notify," // YouTube
+    + "a[href*='plus.google.com/u/0/notifications'],"
+    + "a[href*='plus.google.com/u/1/notifications']"
+    + "{"
+    + "    display: none !important;"
+    + "}";
 
-            if(addedNode.tagName == "BODY") {
+// Because of bug in Chrome match_excludes with CSS files making CSS inline
+var injectCSS = function() {
+    var style = document.createElement("style");
+    style.setAttribute('type', 'text/css');
+    style.innerHTML = css;
+    if(document.body.childNodes.length > 0)
+        document.body.insertBefore(style, document.body.childNodes[0]);
+    else
+        document.body.appendChild(style);
+}
 
-                if(observerForBody)
-                    observerForBody.disconnect();
+if(document.body) {
+    injectCSS();
+}
+else {
+    // Observer which waits when <body> added to document
+    var observerForBody = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            for (var i = 0; i < mutation.addedNodes.length; i++) {
+                var addedNode = mutation.addedNodes[i];
 
-                // Because of bug in Chrome match_excludes with CSS files making CSS inline
-                var style = document.createElement("style");
-                style.setAttribute('type', 'text/css');
-                style.innerHTML =
-                    /*".gb_qa .gb_n,"
-                    + ".gb_ra .gb_n,"
-                    + ".gb_qa .gb_q,"
-                    + ".gb_ra .gb_q,"
-                    + "#gbg1,"
-                    + "#sb-button-notify.yt-uix-button,"
-                    + */"a[href*='plus.google.com/u/0/notifications'],"
-                    + "a[href*='plus.google.com/u/1/notifications']"
-                    + "{"
-                    + "    display: none !important;"
-                    + "}";
-                if(document.body.childNodes.length > 0)
-                    document.body.insertBefore(style, document.body.childNodes[0]);
-                else
-                    document.body.appendChild(style);
+                if(addedNode.tagName == "BODY") {
 
-                observer.observe(document.body, {
-                    childList: true
-                });
+                    if(observerForBody)
+                        observerForBody.disconnect();
 
+                    injectCSS();
+                }
             }
-        }
+        });
     });
-});
-observerForBody.observe(document.documentElement, {
-    childList: true
-});
-
-// Observer which waits when content is loaded
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        for (var i = 0; i < mutation.addedNodes.length; i++) {
-            var addedNode = mutation.addedNodes[i];
-
-            if(addedNode.tagName == "DIV"
-                && (addedNode.className == "vY"            // Gmail
-                )) {
-
-                removeNotifications();
-                if(observer)
-                    observer.disconnect();
-
-            }
-        }
+    observerForBody.observe(document.documentElement, {
+        childList: true
     });
-});
-
-document.onload = function() {
-    if(observerForBody)
-        observerForBody.disconnect();
-    if(observer)
-        observer.disconnect();
-};
-
-function removeNotifications()
-{
-    var potentialElems = document.querySelectorAll(".gb_n, .gb_q, .gb_r, .gb_s");
-    for(var i=0;i<potentialElems.length;i++)
-    {
-        var elem = potentialElems[i];
-        if(elem.href && (elem.href.indexOf("plus.google.com/u/0/notifications") != -1
-            || elem.href.indexOf("plus.google.com/u/1/notifications") != -1))
-        {
-            elem.style.display = "none";
-        }
-    }
 }
